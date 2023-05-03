60489817a7ebe9d0fa0d52faea571d2539f1e438
vermayogendra <yogendra.verma@accolitedigital.com>
2023-05-03 13:52:21 +0530

    usercontroll

--- src/controllers/User_Controllers.js
+++ src/controllers/User_Controllers.js
@@ -189,7 +189,6 @@ exports.get_user_list_by_status_code = async (req, res) => {
         {
           creation_date: { $gte: new Date(process.env.SPECIFIC_DATE) },
         },
-        // { initiate_on_boarding_status: true },
         {
           on_boarding_status: false,
         },
@@ -199,9 +198,6 @@ exports.get_user_list_by_status_code = async (req, res) => {
     });
     const pending_offboarding_users_list = await Zoho_Model.find({
       $and: [
-        // {
-        //   creation_date: { $gte: new Date(process.env.SPECIFIC_DATE) },
-        // },
         { initiate_off_boarding_status: true },
         { off_boarding_status: false },
       ],
