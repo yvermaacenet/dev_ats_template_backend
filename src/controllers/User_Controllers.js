0c9056ef1240ef189e768cf0066a911d13e1fb96
vermayogendra <yogendra.verma@accolitedigital.com>
2023-05-03 14:01:11 +0530

    ...

--- src/controllers/User_Controllers.js
+++ src/controllers/User_Controllers.js
@@ -200,6 +200,7 @@ exports.get_user_list_by_status_code = async (req, res) => {
       $and: [
         { initiate_off_boarding_status: true },
         { off_boarding_status: false },
+        //....
       ],
     }).select({
       token: 0,
