06778823c2b6c63c91daaf64f48c8de099d33ff6
vermayogendra <yogendra.verma@accolitedigital.com>
2023-05-03 13:19:17 +0530

    env

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,13 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 mongoose
-  .connect(
-    // "mongodb://localhost:27017/ats_template"
-    // "mongodb+srv://system:Qwerty%40123@cluster0.6j0zuoz.mongodb.net/awf_development?retryWrites=true&w=majority"
-    // "mongodb+srv://yogendra:AceNet%40123@cluster0.w9ehftd.mongodb.net/awf_dev?retryWrites=true&w=majority"
-    // "mongodb+srv://system:Qwerty%40123@cluster1.uxip7iy.mongodb.net/awf_development?retryWrites=true&w=majority"
-    "mongodb+srv://system:Qwerty%40123@cluster1.uxip7iy.mongodb.net/awf_production?retryWrites=true&w=majority"
-  )
-
+  .connect(`${process.env.DATABASE_CONNECTION_PATH}`)
   .then(() => console.log("Connection is successfull"))
   .catch((err) => `Connection is not successfully - ${err}`);
