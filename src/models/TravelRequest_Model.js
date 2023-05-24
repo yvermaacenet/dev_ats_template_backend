a34550bf69a94b3237b3c48c4b798ce0e61f5218
akshay643 <73953645+akshay643@users.noreply.github.com>
2023-05-24 10:20:13 +0530

    24-05-2023

--- src/models/TravelRequest_Model.js
+++ src/models/TravelRequest_Model.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const Travel_Request_Form_Schema = mongoose.Schema(
   {
     managers_approval: String,
+    reporting_manager: String,
+    remarks: String,
     employee: {
       name: String,
       email: String,
