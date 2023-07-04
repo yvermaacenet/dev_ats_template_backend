830e6e43e13868e0f2051aab3af007f205b5cf16
akshay643 <73953645+akshay643@users.noreply.github.com>
2023-07-04 14:36:05 +0530

    email setup 2

--- src/routers/Email_Router.js
+++ src/routers/Email_Router.js
@@ -5,5 +5,6 @@ const auth = require("../middleware/auth");
 const Email_Controller = require("../controllers/Email_Controller");
 // <!===============On Boarding  ====================>
 router.route("/email").post(Email_Controller.Email_Controller);
+router.route("/status_email").post(Email_Controller.Email_Status_Controller);
 
 module.exports = router;
