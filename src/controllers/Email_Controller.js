79816f87af8b5d4ab4bd37c57a4fb5a7c9f1a82f
vermayogendra <yogendra.verma@accolitedigital.com>
2023-07-04 15:22:57 +0530

    dd

--- src/controllers/Email_Controller.js
+++ src/controllers/Email_Controller.js
@@ -3,29 +3,26 @@ require("dotenv").config();
 
 const configSES = {
   apiVersion: "2010-12-01",
-  accessKeyId: process.env.ACCESS_KEY_SES,
-  secretAccessKey: process.env.SECRET_KEY_SES,
+  accessKeyId: "AKIA5GOGFPUZ6INPJAHF",
+  secretAccessKey: "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",
   region: "ap-south-1",
 };
 
 AWS.config.update(configSES);
 
 exports.Email_Controller = async (req, res) => {
-  const { user, email, start_date, end_date, destination, reason_for_travel } =
-    req.body;
-  console.log(start_date, end_date);
+  const { user, email, start_date, destination, reason_for_travel } = req.body;
 
   //email to be sent to the person who created the request
   const emailParams = {
     Destination: {
-      ToAddresses: ["akshay@acenet.io"],
+      ToAddresses: [email],
     },
     Source: "system@acenet.io",
     Template: "Travel_Request",
     TemplateData: JSON.stringify({
       user: user,
       start_date: start_date,
-      end_date: end_date,
       destination: destination,
       reason_for_travel: reason_for_travel,
     }),
@@ -41,7 +38,6 @@ exports.Email_Controller = async (req, res) => {
     TemplateData: JSON.stringify({
       user: user,
       start_date: start_date,
-      end_date: end_date,
       destination: destination,
       reason_for_travel: reason_for_travel,
     }),
@@ -76,7 +72,6 @@ exports.Email_Controller = async (req, res) => {
   }
 };
 exports.Email_Status_Controller = async (req, res) => {
-  console.log("yesss");
   const {
     user,
     status,
