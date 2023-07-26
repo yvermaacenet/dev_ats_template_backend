c655951cb35b8ce0cb81cfba778752d3a81814d0
akshay643 <73953645+akshay643@users.noreply.github.com>
2023-07-26 19:47:48 +0530

    email

--- src/controllers/Email_Controller.js
+++ src/controllers/Email_Controller.js
@@ -11,69 +11,99 @@ const configSES = {
 AWS.config.update(configSES);
 
 exports.Email_Controller = async (req, res) => {
-  const { user, email, start_date, end_date, destination, reason_for_travel } =
-    req.body;
-  console.log(start_date, end_date);
+  const {
+    user,
+    email,
+    basicDetails,
+    rows,
+    travellersData,
+    accommodationData,
+    roomsData,
+  } = req.body;
 
-  //email to be sent to the person who created the request
-  const emailParams = {
-    Destination: {
-      ToAddresses: ["akshay@acenet.io"],
-    },
-    Source: "system@acenet.io",
-    Template: "Travel_Request",
-    TemplateData: JSON.stringify({
-      user: user,
-      start_date: start_date,
-      end_date: end_date,
-      destination: destination,
-      reason_for_travel: reason_for_travel,
-    }),
-  };
-  //email to be sent to the Managementteam for the approval
+  const recipients = [];
+
+  // Loop through the users and emails arrays to create recipient objects
+  for (let i = 0; i < user.length; i++) {
+    const recipient = {
+      Destination: {
+        ToAddresses: [email[i]],
+      },
+      ReplacementTemplateData: JSON.stringify({
+        user: user[i],
+        basicDetails: basicDetails,
+        rows: rows,
+        travellersData: travellersData,
+        accommodationData: accommodationData,
+        roomsData: roomsData,
+      }),
+    };
+
+    recipients.push(recipient);
+  }
+
+  //email to be sent to the Management team for approval
   const ManagementParams = {
     Destination: {
-      //   ToAddresses: ["amit@acenet.io","manishg@acenet.io","manish@acenet.io", "sunil@acenet.io", "vasanth@acenet.io"],
-      ToAddresses: ["akshay@acenet.io"],
+      ToAddresses: ["leadership@acenet.io"],
     },
     Source: "system@acenet.io",
     Template: "Management_Leave_Request",
     TemplateData: JSON.stringify({
-      user: user,
-      start_date: start_date,
-      end_date: end_date,
-      destination: destination,
-      reason_for_travel: reason_for_travel,
+      basicDetails: basicDetails,
+      rows: rows,
+      travellersData: travellersData,
+      accommodationData: accommodationData,
+      roomsData: roomsData,
     }),
   };
 
   try {
-    let sendPromise = new AWS.SES().sendTemplatedEmail(emailParams).promise();
-    let sendManagementPromise = new AWS.SES()
-      .sendTemplatedEmail(ManagementParams)
-      .promise();
+    if (process.env.ENVIRONMENT === "prod") {
+      const sendBulkEmailParams = {
+        Source: "system@acenet.io",
+        Destinations: recipients,
+        Template: "Travel_Request_New_Template", // Replace with your SES template name
+        DefaultTemplateData: JSON.stringify({
+          user: "",
+          basicDetails: basicDetails,
+          rows: rows,
+          travellersData: travellersData,
+          accommodationData: accommodationData,
+          roomsData: roomsData,
+        }),
+      };
 
-    sendPromise
-      .then(function (data) {
-        console.log(data.MessageId);
-        sendManagementPromise
-          .then(function (data) {
-            console.log(data.MessageId);
-            res.status(201).json({ message: "Email has been sent" });
-          })
-          .catch(function (err) {
-            console.error(err, err.stack);
-            res.status(500).json({
-              message: "Something Went Wrong",
+      let sendPromise = new AWS.SES()
+        .sendBulkTemplatedEmail(sendBulkEmailParams)
+        .promise();
+      let sendManagementPromise = new AWS.SES()
+        .sendTemplatedEmail(ManagementParams)
+        .promise();
+
+      sendPromise
+        .then(function (data) {
+          console.log(data.MessageId);
+          sendManagementPromise
+            .then(function (data) {
+              console.log(data.MessageId);
+              res.status(201).json({ message: "Email has been sent" });
+            })
+            .catch(function (err) {
+              console.error(err, err.stack);
+              res.status(500).json({
+                message: "Something Went Wrong",
+              });
             });
-          });
-      })
-      .catch(function (err) {
-        console.error(err, err.stack);
-        res.status(500).json({ message: "Something Went Wrong" });
-      });
-  }
-  catch (error) {
+        })
+        .catch(function (err) {
+          console.error(err, err.stack);
+          res.status(500).json({ message: "Something Went Wrong" });
+        });
+    } else {
+      res.status(200).json({ message: "Email sent in dev environment" });
+    }
+  } catch (error) {
     console.error("Error creating email template:", error);
   }
 };
@@ -121,7 +151,7 @@ exports.Email_Status_Controller = async (req, res) => {
 
     const ManagementParams = {
       Destination: {
-        ToAddresses: ["akshay@acenet.io"],
+        ToAddresses: ["honey@acenet.io"],
       },
       Source: "system@acenet.io",
       Template: "Groupla_Email_Template",
